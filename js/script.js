4201172b38822df999038863ba706ace0476c64f
Igor Camões <itscamoes@gmail.com>
2019-08-01 03:08:01 -0300

    Arrumar referencias

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ Banner1 = () =>{
     nomeBiblio.innerHTML = 'Biblioteca Arialdo Assis Marques';
     fraseBiblio.innerHTML = '"Sempre me sinto em casa aqui, nunca falta um livro"';
     pessoaFrase.innerHTML = '— Fulano';
-    header.style.backgroundImage = 'url("../media/banner.jpg")';
+    header.style.backgroundImage = 'url("/media/banner.jpg")';
     header.transition = '0.5s';
 }
 
@@ -17,7 +17,7 @@ Banner2 = () =>{
     nomeBiblio.innerHTML = 'Biblioteca Martins Ciqueira';
     fraseBiblio.innerHTML = '"Biblioteca boa que fica perto de casa, meu sobrinho vai lá toda semana"';
     pessoaFrase.innerHTML = '— Cicrano';
-    header.style.backgroundImage = 'url("../media/banner2.jpg")';
+    header.style.backgroundImage = 'url("/media/banner2.jpg")';
     header.transition = '0.5s';
 }
 
